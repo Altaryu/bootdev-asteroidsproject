56753d6131c028280343e1c169425437d0326481
Altaryu <jeffgar4@gmail.com>
2026-02-17 18:53:12 -0500

    Added forward and backward movement to the player.

--- main.py
+++ main.py
@@ -18,14 +18,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            clock.tick(60)
-            dt=clock.tick(60)/1000
+            #clock.tick(60)
             #print(dt)
             
         screen.fill("black")
         player.update(dt)
         player.draw(screen)
         pygame.display.flip()
+
+        dt=clock.tick(60)/1000
             
 
 if __name__ == "__main__":
